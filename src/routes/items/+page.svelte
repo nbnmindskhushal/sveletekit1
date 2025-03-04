b3208474875c328a944cff16ad138fcb4cd6db06
Khushal <Khushal@nbnminds.com>
2025-03-04 13:53:09 +0530

    Fixes

--- src/routes/items/+page.svelte
+++ src/routes/items/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { onMount } from 'svelte';
   
-    let items = [];
+    let items:any = [];
   
     onMount(async () => {
       const res = await fetch('/api/items');
