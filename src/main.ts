cbac8784aebe36ffc135637c625dde5cdba096e8
Khushal <Khushal@nbnminds.com>
2025-03-10 16:49:33 +0530

    Fixes

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import serverless from '@netlify/functions';
+import * as express from 'express';
+import { Handler } from '@netlify/functions';
+import * as serverless from 'serverless-http';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap(): Promise<Handler> {
+  const expressApp = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
   app.enableCors();
   await app.init();
 
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverless.createHandler(expressApp);
+  return serverless(expressApp); // Convert Express to a Serverless Handler
 }
 
-export const handler = bootstrap();
+export const handler: Handler = async (event, context) => {
+  const nestHandler = await bootstrap();
+  return nestHandler(event, context);
+};
