a539a041f11e981c1fbbabb9880e8f62f9c7fab7
Khushal <Khushal@nbnminds.com>
2025-03-10 16:01:41 +0530

    NestJs App

--- src/routes/api/items/+server.js
+++
@@ -1,7 +0,0 @@
-import { json } from '@sveltejs/kit';
-import { prisma } from '$lib/server/prisma';
-
-export async function GET() {
-    const items = await prisma.item.findMany();
-    return json(items);
-}
