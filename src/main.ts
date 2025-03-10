f46b66da938fc92eb33263bb1218b2e29706c932
Khushal <Khushal@nbnminds.com>
2025-03-10 16:46:55 +0530

    netlify.toml

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import serverless from '@netlify/functions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  app.enableCors();
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverless.createHandler(expressApp);
 }
-bootstrap();
+
+export const handler = bootstrap();
